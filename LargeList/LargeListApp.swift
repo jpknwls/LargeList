c14784f14479f06b4fdf5bd88bedb8647b63c16d
overlair <overlair@gmail.com>
2024-06-24 19:57:30 -0700

    Initial Commit

---
+++ LargeList/LargeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LargeListApp.swift
+//  LargeList
+//
+//  Created by John Knowles on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LargeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
